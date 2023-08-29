944e11dfc91ddcf23d096a8c63d479724c4adbe6
Mahmoud Ibrahim <mahmoud28@gmail.com>
2023-08-29 18:16:26 +0200

    Remove ContentView

--- ShackleHotelBuddy/ShackleHotelBuddy/ShackleHotelBuddyApp.swift
+++ ShackleHotelBuddy/ShackleHotelBuddy/ShackleHotelBuddyApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct ShackleHotelBuddyApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Group {
+                HotelSearchView()
+            }
         }
     }
 }
